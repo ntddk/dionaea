c734cd344f3cb4abf5ac6f868eaccc9496dafd4f
ntddk <ntinternal@gmail.com>
2017-07-10 13:56:40 +0000

    Anti-Nmap

--- modules/python/scripts/ftp.py
+++ modules/python/scripts/ftp.py
@@ -224,7 +224,7 @@ class ftpd(connection):
 
 	def handle_established(self):
 		self.processors()
-		self.reply(WELCOME_MSG, "Welcome to the ftp service")
+		self.reply(WELCOME_MSG, "Microsoft FTP Service")
 
 	def handle_io_in(self, data):
 #		try:
